c374d22df4d0bc712939e3648e2f970fac6203e4
Edwin <edwin.kaisers0451@gmail.com>
2021-11-13 20:39:29 +0100

    Initial commit

---
+++ models/post.js
@@ -0,0 +1,13 @@
+const { text } = require("express");
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var PostSchema = new Schema({
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  timestamp: { type: Date, required: true },
+  published: { type: Boolean, required: true },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
